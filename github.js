d33568db576f3365dee1ac7eb4f3c92cc48b3013
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-10-28 11:35:53 -0400

    [github.js] Display json data on screen

--- github.js
+++ github.js
@@ -1,9 +1,29 @@
-// fetch('https://api.github.com/users/jjuriz')
-// .then(response => {
-//     //console.log(response);
-//     return response.json();
-// })
-// .then(githubData => {
-//     console.log(githubData)
-// });
+const $container = document.querySelector('.container');
+
+fetch('https://api.github.com/users/jjuriz')
+.then(response => {
+    //console.log(response);
+    return response.json();
+})
+.then(githubData => {
+    const jeremyObj = {
+        bio: githubData.bio,
+        username: githubData.login,
+        name: githubData.name
+    }
+    console.log(jeremyObj)
+    // console.log(githubData)
+
+    const $newElement = document.createElement('p');
+    $newElement.textContent = jeremyObj.name;
+    // append new element to container
+    $container.appendChild($newElement);
+    
+})
+.catch(error => {
+    console.log(error)
+})
+
+
+
 
