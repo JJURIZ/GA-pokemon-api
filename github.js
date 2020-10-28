08acc56ac93e2eb04f45139e24dbb0afa5d1e0da
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-10-28 11:07:52 -0400

    [sapcex] Getting API data.

---
+++ github.js
@@ -0,0 +1,9 @@
+// fetch('https://api.github.com/users/jjuriz')
+// .then(response => {
+//     //console.log(response);
+//     return response.json();
+// })
+// .then(githubData => {
+//     console.log(githubData)
+// });
+
